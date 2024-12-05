35dffb485c599e27ec1245c3d3b4c6b5fdf860b1
MAlutinS <gardenman0war@gmail.com>
2024-12-05 23:25:30 +0300

    appmodule upd

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,28 +1,37 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
-import { TeamModule } from '../team/team.module';
+import { WorkerModule } from '../worker/worker.module';
+import { BrigadeModule } from '../brigade/brigade.module';
 import { DepartmentModule } from '../department/department.module';
-import { EmployeeModule } from '../employee/employee.module';
-import { TrainModule } from '../train/train.module';
 import { LocomotiveModule } from '../locomotive/locomotive.module';
-import { CarriageModule } from '../carriage/carriage.module';
-import { WaybillModule } from '../waybill/waybill.module';
-import { LuggageModule } from '../luggage/luggage.module';
+import { TrainModule } from '../train/train.module';
+import { WagonModule } from '../wagon/wagon.module';
+import { TravelSheetModule } from '../travel_sheet/travel_sheet.module';
 import { RouteModule } from '../route/route.module';
-import { PassengerModule } from '../passenger/passenger.module';
 import { TicketModule } from '../ticket/ticket.module';
-import { CashierModule } from '../cashier/cashier.module';
+import { PassengerModule } from '../passenger/passenger.module';
 import { DocumentModule } from '../document/document.module';
+import { LuggageModule } from '../luggage/luggage.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  }) , PrismaModule, TeamModule,
-  DepartmentModule, EmployeeModule, TrainModule,
-  LocomotiveModule, CarriageModule, WaybillModule,
-  LuggageModule, RouteModule, PassengerModule,
-  TicketModule, CashierModule, DocumentModule
-],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    WorkerModule,
+    BrigadeModule,
+    DepartmentModule,
+    LocomotiveModule,
+    TrainModule,
+    WagonModule,
+    TravelSheetModule,
+    RouteModule,
+    TicketModule,
+    PassengerModule,
+    DocumentModule,
+    LuggageModule,
+  ],
 })
 export class AppModule {}
