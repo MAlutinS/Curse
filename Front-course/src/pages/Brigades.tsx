ae945d56a906f41417260b16701fcad332fd44fb
MAlutinS <gardenman0war@gmail.com>
2024-12-09 21:23:03 +0300

    Add front

---
+++ Front-course/src/pages/Brigades.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Brigades: FC = () => {
+    return <h1>Brigades</h1>
+}
+
+export default Brigades
---
+++ Front-course/src/pages/Workers.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Workers: FC = () => {
+    return <h1>Workers</h1>
+}
+
+export default Workers
